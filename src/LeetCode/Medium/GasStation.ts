950bdc19eb8a043145f0a87b87a1c07648f0cd0c
Andrew213 <a.kochanov31@yandex.ru>
2026-04-06 20:24:55 +0300

    gasStation

---
+++ src/LeetCode/Medium/GasStation.ts
@@ -0,0 +1,46 @@
+/**
+ * 
+// There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+// You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. 
+// You begin the journey with an empty tank at one of the gas stations.
+
+// Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction, 
+// otherwise return -1. If there exists a solution, it is guaranteed to be unique.
+ */
+
+// Решение в лоб. Подумать над оптимизацией
+function makeCircle(
+  startGasStationIndex: number,
+  gas: number[],
+  cost: number[],
+) {
+  let capacity = 0;
+
+  for (let i = 0; i < gas.length; i++) {
+    const index = (startGasStationIndex + i) % gas.length;
+    capacity += gas[index];
+
+    const gasRemains = capacity - cost[index];
+    if (gasRemains < 0) {
+      return -1;
+    }
+    capacity = gasRemains;
+  }
+
+  return startGasStationIndex;
+}
+
+function canCompleteCircuit(gas: number[], cost: number[]): number {
+  let startIndex = -1;
+  for (let i = 0; i < gas.length; i++) {
+    startIndex = makeCircle(i, gas, cost);
+    if (startIndex >= 0) {
+      break;
+    }
+  }
+
+  return startIndex;
+}
+
+console.log("result", canCompleteCircuit([2, 3, 4], [3, 4, 3]));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 // import "./150 interview questions/ReverseWordInAString";
 // import "./YandexContest/TwoSum";
 // import "./LeetCode/Easy/RansomeNotes";
-import "./LeetCode/Easy/MiddleOfTheLinkedList";
+// import "./LeetCode/Easy/MiddleOfTheLinkedList";
+import "./LeetCode/Medium/GasStation";
