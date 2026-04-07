96a106c860163a39dceb141299b08fd60b154cf0
Andrew213 <a.kochanov31@yandex.ru>
2026-04-07 12:34:26 +0300

    gasStation

--- src/LeetCode/Medium/GasStation.ts
+++ src/LeetCode/Medium/GasStation.ts
@@ -10,37 +10,61 @@
  */
 
 // Решение в лоб. Подумать над оптимизацией
-function makeCircle(
-  startGasStationIndex: number,
-  gas: number[],
-  cost: number[],
-) {
-  let capacity = 0;
+// function makeCircle(
+//   startGasStationIndex: number,
+//   gas: number[],
+//   cost: number[],
+// ) {
+//   let capacity = 0;
 
-  for (let i = 0; i < gas.length; i++) {
-    const index = (startGasStationIndex + i) % gas.length;
-    capacity += gas[index];
+//   for (let i = 0; i < gas.length; i++) {
+//     const index = (startGasStationIndex + i) % gas.length;
+//     capacity += gas[index];
 
-    const gasRemains = capacity - cost[index];
-    if (gasRemains < 0) {
-      return -1;
-    }
-    capacity = gasRemains;
-  }
+//     const gasRemains = capacity - cost[index];
+//     if (gasRemains < 0) {
+//       return -1;
+//     }
+//     capacity = gasRemains;
+//   }
 
-  return startGasStationIndex;
-}
+//   return startGasStationIndex;
+// }
+
+// function canCompleteCircuit(gas: number[], cost: number[]): number {
+//   let startIndex = -1;
+//   for (let i = 0; i < gas.length; i++) {
+//     startIndex = makeCircle(i, gas, cost);
+//     if (startIndex >= 0) {
+//       break;
+//     }
+//   }
+
+//   return startIndex;
+// }
 
 function canCompleteCircuit(gas: number[], cost: number[]): number {
-  let startIndex = -1;
+  let capacity = 0;
+  let startGasStationIndex = 0;
+  // общий баланс топлива по всему кругу;
+  let total = 0;
+
   for (let i = 0; i < gas.length; i++) {
-    startIndex = makeCircle(i, gas, cost);
-    if (startIndex >= 0) {
-      break;
+    capacity += gas[i];
+    total += gas[i] - cost[i];
+    const gasRemains = capacity - cost[i];
+    if (gasRemains < 0) {
+      //   res = -1;
+      capacity = 0;
+      //двигаю начальную станцию если запоролся
+      startGasStationIndex = i + 1;
+    } else {
+      // коплю топливо
+      capacity = gasRemains;
     }
   }
 
-  return startIndex;
+  return total < 0 ? -1 : startGasStationIndex;
 }
 
-console.log("result", canCompleteCircuit([2, 3, 4], [3, 4, 3]));
+console.log("result", canCompleteCircuit([2, 3, 4], [3, 4, 3])); // O(N)
